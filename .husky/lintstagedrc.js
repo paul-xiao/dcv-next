e9bd9ec5e9d7cdffc5ca33ea2314a4b1d2452870
肖波 <xiaoboc@dcits.com>
2022-03-24 16:31:28 +0800

    chore: add pretty-quick

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -1,11 +1,10 @@
 module.exports = {
-  "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write", "git add"],
+  "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
   "{!(package)*.json,*.code-snippets,.!(browserslist)*rc}": [
     "prettier --write--parser json",
-    "git add",
   ],
   "package.json": ["prettier --write"],
-  "*.vue": ["eslint --fix", "prettier --write", "stylelint --fix", "git add"],
-  "*.{scss,less,styl}": ["stylelint --fix", "prettier --write", "git add"],
-  "*.md": ["prettier --write", "git add"],
+  "*.vue": ["eslint --fix", "prettier --write", "stylelint --fix"],
+  "*.{scss,less,styl}": ["stylelint --fix", "prettier --write"],
+  "*.md": ["prettier --write"],
 };
